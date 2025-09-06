77b29f0c9e7488fecad43da34ccd04c8d7d68817
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-06 10:14:07 +0800

    commit

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the program!");
+
+        
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}");
+
+        
+        Console.Write("Please enter your favorite number: ");
+        string numberInput = Console.ReadLine();
+
+        int favoriteNumber;
+        if (int.TryParse(numberInput, out favoriteNumber))
+        {
+            int square = favoriteNumber * favoriteNumber;
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid number entered. Please enter a valid integer.");
+        }
     }
 }
