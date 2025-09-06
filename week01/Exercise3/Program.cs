6a5cebd28e67a9fb4588378af453096ef0c9b399
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-06 08:22:18 +0800

    commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        int magicNumber = 10; // The magic number
+        int guess = 0;
+
+        Console.WriteLine("What is the magic number?.");
+
+        // Keep looping until the correct number is guessed
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine(" You guessed it!");
+            }
+        }
     }
-}
+}         
+    
+
