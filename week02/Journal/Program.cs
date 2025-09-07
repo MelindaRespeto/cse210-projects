3c8f763921f02a42ca59de13221d75c93002a5bc
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-07 13:26:11 +0800

    commit

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+        
+        public class House
+    {
+         public string _owner;
+         public Blind _kitchen;
+         public Blind _livingRoom;
+    }
     }
 }
