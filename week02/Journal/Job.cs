4cef599e7ca60a8a7226ffc0f8b61e79bb87aaf5
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-07 13:56:17 +0800

    commit

--- week02/Journal/Job.cs
+++ week02/Journal/Job.cs
@@ -12,6 +12,9 @@ class Program
          public string _company;
          public int _startYear;
          public int _endYear;
+         {
+             Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+         }
          
     }
     }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,27 +1,31 @@
 using System;
 
-class Program
+{class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+        
         Job job1 = new Job();
         job1._jobTitle = "Teacher Assistant";
         job1._company = "Department of Education";
         job1._startYear = 2022;
         job1._endYear = 2024;
+
+        Job job2 = new Job();
         job2._jobTitle = "Qaulity Assurance";
         job2._company = "Astec Company";
         job2._startYear = 2019;
         job2._endYear = 2020;
 
-    public void DisplayJobDetails()
+        List<Job> jobs = new List<Job>();
+        jobs.Add(job1);
+        jobs.Add(job2);
 
-    {   Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-        job1.DisplayJobDetails();
-        job2.DisplayJobDetails();
+        foreach (Job job in jobs)
+    
 
-
-        
-    }
+        {  job.DisplayJobDetails();
+        }
     
     }
+}
