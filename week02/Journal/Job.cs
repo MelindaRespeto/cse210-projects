93b963fb1126d3908e4ce91b37457271043c83a8
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-07 13:24:50 +0800

    commit

---
+++ week02/Journal/Job.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World! This is the Journal Project.");
+        
+        public class Job
+    {
+         public string _jobTitle;
+         public string _company;
+         public int _startYear;
+         public int _endYear;
+         
+    }
+    }
+}
