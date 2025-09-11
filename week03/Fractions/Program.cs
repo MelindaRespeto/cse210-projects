a34d1731028198a6ef6e55883913c91766daad91
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-11 17:14:46 +0800

    commit

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction frac = new Fraction(8, 12);
+
+        Console.Write("Original fraction: ");
+        frac.Display();
+
+        frac.Simplify();
+        Console.Write("Simplified fraction: ");
+        frac.Display();
+
+        // Changing values using setters
+        frac.SetNumerator(15);
+        frac.SetDenominator(20);
+        Console.Write("Updated fraction: ");
+        frac.Display();
+
+        frac.Simplify();
+        Console.Write("Simplified updated fraction: ");
+        frac.Display();
     }
 }
+
+
