4220d9bd11b336234a07e14e9ebc4231c85053d4
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-20 08:11:45 +0800

    Create Word.cs

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,24 @@
+namespace ScriptureMemorizer
+{
+    class Word
+    {
+        private string _text;
+        public bool IsHidden { get; private set; }
+
+        public Word(string text)
+        {
+            _text = text;
+            IsHidden = false;
+        }
+
+        public void Hide()
+        {
+            IsHidden = true;
+        }
+
+        public string GetDisplayText()
+        {
+            return IsHidden ? new string('_', _text.Length) : _text;
+        }
+    }
+}
