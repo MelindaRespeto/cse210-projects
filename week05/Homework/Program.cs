c8385a26d0711c5aa833f50d5fc918d5d9f5fd7e
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-27 07:36:26 +0800

    Update Program.cs

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        //create a MathAssignment object
+        MathAssignment math =new MathAssignment("Maganda", "Fractions", "7.3", "10-20");
+        Console. WriteLine(Math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // Create a WritingAssignment object
+        WritingAssignment writing = new WritingAssignment("Ghie", "History", "The Philippines Croc.");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
