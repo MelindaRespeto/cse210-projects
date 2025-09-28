62b3c45a9692441a9ae37d01463a98a4a97e8180
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-28 14:40:44 +0800

    Update WritingAssignment.cs

--- week05/Homework/WritingAssignment.cs
+++ week05/Homework/WritingAssignment.cs
@@ -1,1 +1,13 @@
-
+public class Writing Assignment: Assignment
+{ 
+public string Title { get; set; }
+public WritingAssignment(string studentName, string topic, string title);
+     :base ( studentName, topic);
+     {
+     Title =title;
+     }
+     public string GetWritingInformation()
+    {
+        return $"{Title} by {StudentName}";
+    }
+}
