6d66adc654f0d8be2c7816531ed6adca341dce9c
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-10-03 09:04:35 +0800

    Update Program.cs

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Listing Activity");
+            Console.WriteLine("3. Reflection Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("\nSelect a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    new BreathingActivity().Run();
+                    break;
+                case "2":
+                    new ListingActivity().Run();
+                    break;
+                case "3":
+                    new ReflectionActivity().Run();
+                    break;
+                case "4":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
     }
 }
