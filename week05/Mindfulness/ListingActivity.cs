bcb2692e44f23a669973bc693375a022f28484a0
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-10-03 09:03:08 +0800

    Update ListingActivity.cs

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -3,28 +3,26 @@ using System.Collections.Generic;
 
 public class ListingActivity : Activity
 {
-    private List<string> _prompts = new List<string>
+    private List<string> _prompts = new List<string>()
     {
-        "Who are people that you appreciate?",
+        "Who are people you appreciate?",
         "What are personal strengths of yours?",
         "Who are people that you have helped this week?",
-        "When have you felt the Holy Spirit this month?",
-        "Who are some of your personal heroes?"
+        "When have you felt peace this month?"
     };
 
-    public ListingActivity()
-        : base("Listing Activity",
-              "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.") { }
+    public ListingActivity() 
+        : base("Listing Activity", "This activity will help you reflect on the good things in your life by listing items.") { }
 
-    public void Run()
+    public override void Run()
     {
-        DisplayStartingMessage();
+        Start();
 
         Random rand = new Random();
         string prompt = _prompts[rand.Next(_prompts.Count)];
-        Console.WriteLine($"\nList as many responses as you can to the following prompt:");
-        Console.WriteLine($"--- {prompt} ---");
-        Console.WriteLine("You may begin in:");
+        Console.WriteLine($"\nList as many responses as you can to the following prompt:\n--- {prompt} ---");
+
+        Console.WriteLine("\nYou may begin in:");
         ShowCountdown(5);
 
         List<string> responses = new List<string>();
@@ -33,14 +31,11 @@ public class ListingActivity : Activity
         while (DateTime.Now < endTime)
         {
             Console.Write("> ");
-            string response = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(response))
-            {
-                responses.Add(response);
-            }
+            responses.Add(Console.ReadLine());
         }
 
         Console.WriteLine($"\nYou listed {responses.Count} items!");
-        DisplayEndingMessage();
+        End();
     }
 }
+
